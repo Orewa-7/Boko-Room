bf2424a6235b842c2b08856cf641783c323f0b78
abdou abdou <abdou-elboursoumi@outlook.fr>
2022-08-04 08:01:15 +0200

    cross product

--- src/Experience/World/Controls.js
+++ src/Experience/World/Controls.js
@@ -25,6 +25,10 @@ export default class Controls
         this.position = new THREE.Vector3(0, 0, 0)
         this.lookAtPosition = new THREE.Vector3(0, 0, 0)
 
+        this.directionalVector = new THREE.Vector3(0, 0, 0)
+        this.staticVector = new THREE.Vector3(0, -1, 0)
+        this.crossVector = new THREE.Vector3(0, 0, 0)
+
     
         this.setPath()
         this.onWheel()
@@ -33,11 +37,10 @@ export default class Controls
 
     setPath(){
         this.curve = new THREE.CatmullRomCurve3( [
-            new THREE.Vector3( -10, 0, 10 ),
-            new THREE.Vector3( -5, 5, 5 ),
-            new THREE.Vector3( 0, 0, 0 ),
-            new THREE.Vector3( 5, -5, 5 ),
-            new THREE.Vector3( 10, 0, 10 )
+            new THREE.Vector3( -5, 0, 0 ),
+            new THREE.Vector3( 0, 0, -5 ),
+            new THREE.Vector3( 5, 0, 0 ),
+            new THREE.Vector3( 0, 0, 5 ),
         ], true );
         
         const points = this.curve.getPoints( 50 );
@@ -72,19 +75,37 @@ export default class Controls
             this.lerp.target,
             this.lerp.ease
         )
-        if(this.back){
-            this.lerp.target -= 0.001
-        } else {
-            this.lerp.target += 0.001
-        }
-        this.lerp.target = gsap.utils.clamp(0, 1, this.lerp.target)
-        this.lerp.current = gsap.utils.clamp(0, 1, this.lerp.current)
 
-        this.curve.getPointAt(this.lerp.current, this.position)
-        this.curve.getPointAt(this.lerp.current + 0.00001, this.lookAtPosition)
 
+        this.curve.getPoint(this.lerp.current % 1, this.position)
         this.camera.orthographicCamera.position.copy(this.position)
-        this.camera.orthographicCamera.lookAt(this.lookAtPosition)
+
+        this.directionalVector.subVectors(
+            this.curve.getPointAt((this.lerp.current % 1) + 0.000001), 
+            this.position
+        )
+
+        this.directionalVector.normalize()
+
+        this.crossVector.crossVectors(this.directionalVector, this.staticVector)
+
+        this.crossVector.multiplyScalar(10000)
+
+        this.camera.orthographicCamera.lookAt(this.crossVector)
+
+
+        // if(this.back){
+        //     this.lerp.target -= 0.001
+        // } else {
+        //     this.lerp.target += 0.001
+        // }
+        // this.lerp.target = gsap.utils.clamp(0, 1, this.lerp.target)
+        // this.lerp.current = gsap.utils.clamp(0, 1, this.lerp.current)
+
+        // this.curve.getPointAt(this.lerp.current, this.position)
+        // this.curve.getPointAt(this.lerp.current + 0.00001, this.lookAtPosition)
+
+        // this.camera.orthographicCamera.position.copy(this.position)
+        // this.camera.orthographicCamera.lookAt(this.lookAtPosition)
     }
 }
-// 3 46 16
