1a254b5cdd5f0fa619e3c991de0c720cc9d21fba
abdou abdou <abdou-elboursoumi@outlook.fr>
2022-08-09 10:40:01 +0200

    edited the blender model + scrolltriger

--- src/Experience/World/Controls.js
+++ src/Experience/World/Controls.js
@@ -15,27 +15,93 @@ export default class Controls
         this.resources = this.experience.resources
         this.time = this.experience.time
         this.room = this.experience.world.room.actualRoom
+        this.room.children.forEach(child => {
+            if(child.type === "RectAreaLight"){
+                this.rectLight = child
+            }
+        })
         gsap.registerPlugin(ScrollTrigger)
 
-        this.setPath()
+        this.scrollTrigger()
 
     }
 
-    setPath(){
-        this.timeline = new gsap.timeline()
-        this.timeline.to(this.room.position,{
-            x: () => {
-                 return this.sizes.width * 0.00098
+    scrollTrigger(){
+        // this.timeline = new gsap.timeline()
+        // this.timeline.to(this.room.position,{
+        //     x: () => {
+        //          return this.sizes.width * 0.00098
+        //     },
+        //     scrollTrigger:{
+        //         trigger: '.first-move',
+        //         markers: true,
+        //         start: "top top",
+        //         end: "bottom bottom",
+        //         scrub: 0.6,
+        //         invalidateOnRefresh: true 
+        //     }
+        // })
+
+        ScrollTrigger.matchMedia({
+	
+            // Desktop
+            "(min-width: 969px)": () => {
+                // First Section ------------------------------------------
+                this.firstMoveTimeline = new gsap.timeline({
+                    scrollTrigger:{
+                        trigger: '.first-move',
+                        start: 'top top',
+                        end: 'bottom bottom',
+                        scrub: 0.6,
+                        invalidateOnRefresh: true
+                    }
+                })
+                this.firstMoveTimeline.to(this.room.position, {
+                    x: () =>{
+                        return this.sizes.width * 0.00098
+                    }
+                })
+
+                // Second Section  -----------------------------------------
+                this.secondMoveTimeline = new gsap.timeline({
+                    scrollTrigger:{
+                        trigger: '.second-move',
+                        start: 'top top',
+                        end: 'bottom bottom',
+                        scrub: 0.6,
+                        invalidateOnRefresh: true
+                    }
+                })
+                this.secondMoveTimeline.to(this.room.position, {
+                    x: () =>{
+                        return 1
+                    },
+                    z: () =>{
+                        return this.sizes.height * 0.00098
+                    }
+                }, 'same')
+                this.secondMoveTimeline.to(this.room.scale, {
+                    x: 0.4,
+                    y: 0.4,
+                    z: 0.4,
+                }, 'same')
+                this.secondMoveTimeline.to(this.rectLight, {
+                    width: 0.2 * 4,
+                    height: 0.55 * 4
+                }, 'same')
+
             },
-            scrollTrigger:{
-                trigger: '.first-move',
-                markers: true,
-                start: "top top",
-                end: "bottom bottom",
-                scrub: 0.6,
-                invalidateOnRefresh: true 
+
+            // Mobile 
+            "(max-width: 968px)": () => {
+
+            },
+              
+            // all 
+            "all": function() {
             }
-        })
+              
+          }); 
     }
 
     reseize(){}
--- src/Experience/World/Room.js
+++ src/Experience/World/Room.js
@@ -26,6 +26,8 @@ export default class Room {
     }
 
     setModel() {
+        console.log(this.actualRoom.children)
+
         this.actualRoom.children.forEach((child) => {
             child.castShadow = true
             child.receiveShadow = true
@@ -36,17 +38,17 @@ export default class Room {
                     groupChild.receiveShadow = true
                 })
             }
-            if (child.name === "AquaGlass") {
-                child.material = new THREE.MeshPhysicalMaterial()
-                child.material.roughness = 0
-                child.material.color.set(0x549dd2)
-                child.material.ior = 3
-                child.material.transmission = 1
-                child.material.opacity = 1
+            if (child.name === "Aquarium") {
+                child.children[0].material = new THREE.MeshPhysicalMaterial()
+                child.children[0].material.roughness = 0
+                child.children[0].material.color.set(0x549dd2)
+                child.children[0].material.ior = 3
+                child.children[0].material.transmission = 1
+                child.children[0].material.opacity = 1
             }
-            if (child.name === "Screen") {
-                child.material = new THREE.MeshBasicMaterial()
-                child.material.map = this.resources.items.OnePiece
+            if (child.name === "Computer") {
+                child.children[1].material = new THREE.MeshBasicMaterial()
+                child.children[1].material.map = this.resources.items.OnePiece
             }
         })
 
